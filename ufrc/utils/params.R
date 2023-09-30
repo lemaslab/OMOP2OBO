d2a80a6bc5405f60e5a7f13467bba65e3cc69602
Dominick J. Lemas <dominicklemas@gmail.com>
2023-09-30 12:31:56 -0400

    troubleshooting

---
+++ ufrc/utils/params.R
@@ -0,0 +1,17 @@
+
+# R libraries
+#------------
+
+library(readr)
+library(tidyverse)
+library(stringr)
+
+# import data
+#------------
+
+concept <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release/concept_v2.0.txt", show_col_types = FALSE) 
+vocabulary <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release/vocabulary_v2.0.txt", show_col_types = FALSE) 
+concept_ancestor <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release/concept_ancestor_v2.0.txt", show_col_types = FALSE) %>% rename(concept_id=ancestor_concept_id)
+#concept_synonym <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release/concept_synonym_v2.0.txt", show_col_types = FALSE) 
+
+# notes: https://ohdsi.github.io/CommonDataModel/cdm60.html#VOCABULARY
