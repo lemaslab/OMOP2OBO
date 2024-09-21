90056981b1772db8f8358c7f22209c7061ad13d2
Dominick J. Lemas <dominicklemas@gmail.com>
2024-09-21 12:39:01 -0400

    chatgpt translation of SQL code

--- ufrc/utils/params.R
+++ ufrc/utils/params.R
@@ -9,9 +9,10 @@ library(stringr)
 # import data
 #------------
 
-concept <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release/concept_v2.0.txt", show_col_types = FALSE) 
-vocabulary <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release/vocabulary_v2.0.txt", show_col_types = FALSE) 
+condition_occurrence <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release//condition_occurrence_v2.1.txt", show_col_types = FALSE)
+concept <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release/concept_v2.0.txt", show_col_types = FALSE)
+vocabulary <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release/vocabulary_v2.0.txt", show_col_types = FALSE)
 concept_ancestor <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release/concept_ancestor_v2.0.txt", show_col_types = FALSE) %>% rename(concept_id=ancestor_concept_id)
-#concept_synonym <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release/concept_synonym_v2.0.txt", show_col_types = FALSE) 
+concept_synonym <- read_tsv(file="/blue/djlemas/share/data/Benchmark/OMOP/data_release/concept_synonym_v2.0.txt", show_col_types = FALSE)
 
 # notes: https://ohdsi.github.io/CommonDataModel/cdm60.html#VOCABULARY
